a198646e72e3c00150811643538585a2c3eebca9
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-09-01 21:47:12 +0600

    problem solved

--- BobAliceCF.playground/Contents.swift
+++ BobAliceCF.playground/Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var alice: [Int] = [5, 6, 7]
+var bob: [Int] = [3, 6, 10]
+var ar: [Int] = [Int]()
+
+var alineCount = 0
+var bobCount = 0
+
+for i in 0..<alice.count {
+    if alice[i] > bob[i]{
+        alineCount += 1
+    }
+    if alice[i] < bob[i]{
+        bobCount += 1
+    }
+}
+ar.append(alineCount)
+ar.append(bobCount)
+print(ar)
